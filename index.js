7c6405b86c207f8110c62aef1ea3fb02c6902040
Pranit Bauva <pranit@bauva.com>
2023-03-01 22:30:10 +0530

    point to prod

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 async function sendEmailAddressAPI(emailAddress) {
-  //const url = 'https://landing-page-email-address-input-fmdaixa45q-el.a.run.app/add-email-address';
-  const url = 'http://localhost:8080/add-email-address';
+  const url = 'https://landing-page-email-address-input-fmdaixa45q-el.a.run.app/add-email-address';
+  //const url = 'http://localhost:8080/add-email-address';
   const response = await fetch(url, {
     method: 'POST',
     mode: 'no-cors',
