2b7967ddc9ab27d1414b9f5eb4235e0b61c73ba5
Pranit Bauva <pranit@bauva.com>
2023-03-01 22:25:07 +0530

    add xypnox's comments and POST as form data instead of json

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 async function sendEmailAddressAPI(emailAddress) {
-  const data = {
-    'emailAddress': emailAddress
-  };
-  const response = await fetch('https://landing-page-email-address-input-fmdaixa45q-el.a.run.app/add-email-address', {
+  //const url = 'https://landing-page-email-address-input-fmdaixa45q-el.a.run.app/add-email-address';
+  const url = 'http://localhost:8080/add-email-address';
+  const response = await fetch(url, {
     method: 'POST',
+    mode: 'no-cors',
+    cache: 'no-cache',
+    credentials: 'omit',
     headers: {
-      'Content-Type': 'application/json',
+      'Content-Type': 'application/x-www-form-urlencoded',
     },
-    body: JSON.stringify(data),
+    body: 'emailAddress=' + emailAddress,
   });
   return response.json();
 }
