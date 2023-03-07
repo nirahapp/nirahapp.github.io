1100bfc03c89e6cb85bd3d10750238a41a9c4a52
Pranit Bauva <pranit@bauva.com>
2023-03-07 21:11:30 +0530

    validate the input, if invalid, disable button click

--- index.js
+++ index.js
@@ -14,8 +14,37 @@ async function sendEmailAddressAPI(emailAddress) {
   return response;
 }
 
+function validateEmail(emailRaw) {
+  const email = emailRaw.trim();
+  if (email.length <= 0) {
+    return false;
+  }
+  // TODO: add other checks
+  return true;
+}
+
+function handleEmailSubmit() {
+  let emailAddressInput = document.getElementById('email-address-input');
+  const email = emailAddressInput.value;
+  if (validateEmail(email) === true) {
+    setEmailSubmit(true);
+  } else {
+    setEmailSubmit(false);
+  }
+}
+
+function setEmailSubmit(clickable) {
+  let emailAddressInput = document.getElementById('email-submit-button');
+  if (clickable === true) {
+    emailAddressInput.disabled = false;
+  } else {
+    emailAddressInput.disabled = true;
+  }
+  
+}
+
 async function handleSubmit() {
-  emailAddressInput = document.getElementById('emailAddress');
+  let emailAddressInput = document.getElementById('email-address-input');
   sendEmailAddressAPI(emailAddressInput.value).then((data) => {
     emailForm = document.getElementById("email-form");
     thankYou = document.getElementById("submission-thank-you");
