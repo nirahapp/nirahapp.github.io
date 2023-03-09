8e981ceccfa0cf8e5222e49dd8da1b05d0ae31ec
Pranit Bauva <pranit@bauva.com>
2023-03-10 00:54:40 +0530

    email form before value proposition
    It's difficult to get readers' attention till the end, ask for
    email in between and then tell your value proposition.
    
    Also use `setInterval` to run the email validator every 100ms

--- index.js
+++ index.js
@@ -15,15 +15,17 @@ async function sendEmailAddressAPI(emailAddress) {
 }
 
 function validateEmail(emailRaw) {
-  const email = emailRaw.trim();
+  const email = emailRaw.trim().toLowerCase();
   if (email.length <= 0) {
+    return false;
+  } else if (!email.match( /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
     return false;
   }
   // TODO: add other checks
   return true;
 }
 
-function handleEmailSubmit() {
+function verifyAndSetEmail() {
   let emailAddressInput = document.getElementById('email-address-input');
   const email = emailAddressInput.value;
   if (validateEmail(email) === true) {
@@ -37,8 +39,10 @@ function setEmailSubmit(clickable) {
   let emailAddressInput = document.getElementById('email-submit-button');
   if (clickable === true) {
     emailAddressInput.disabled = false;
+    document.getElementById('email-submit-button').className = 'leading-6 rounded-lg p-2 bg-green-700 hover:outline hover:outline-gray-500';
   } else {
     emailAddressInput.disabled = true;
+    document.getElementById('email-submit-button').className = 'leading-6 rounded-lg p-2 bg-gray-200 hover:outline-none';
   }
   
 }
@@ -53,3 +57,5 @@ async function handleSubmit() {
     console.log(data);
   });
 }
+
+setInterval(verifyAndSetEmail, 100);
